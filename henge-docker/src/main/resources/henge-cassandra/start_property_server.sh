4a8825745d268f2b0894e490adac7f83fc593972
kbober <kbober@kenzan.com>
2016-10-25 17:01:06 -0400

    Resolved issues with the henge-docker container setup and the henge-service docker plugin setup

---
+++ henge-docker/src/main/resources/henge-cassandra/start_property_server.sh
@@ -0,0 +1,12 @@
+echo '==================='
+echo 'Poop is in the eye of the deholder'
+env
+echo $cassandra_ip
+echo '==================='
+
+cassandra_port=9042
+load_file="/tmp/load.cql"
+cql_version="3.4.0"
+cqlsh -f $load_file cassandra_server $cassandra_port --cqlversion=$cql_version
+
+/usr/bin/java -Dspring.profiles.active=dev,cassandra,setmapping -Dcassandra.host=cassandra_server -Dcassandra.port=$cassandra_port -jar /tmp/henge.jar
